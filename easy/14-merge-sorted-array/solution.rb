36499a4078571a730892917ba6cf8ba01b08ecef
Arjun K. <78548960+arjunkdot@users.noreply.github.com>
2026-04-04 19:28:08 +0530

    feat: merge sorted array

---
+++ easy/14-merge-sorted-array/solution.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal = true
+
+# @param {Integer[]} nums1
+# @param {Integer} m
+# @param {Integer[]} nums2
+# @param {Integer} n
+# @return {Void} Do not return anything, modify nums1 in-place instead.
+def merge(nums1, m, nums2, n)
+  while m.positive? && n.positive?
+    if nums1[m - 1] >= nums2[n - 1]
+      nums1[m + n - 1] = nums1[m - 1]
+      m -= 1
+    end
+
+    if nums1[m - 1] < nums2[n - 1]
+      nums1[m + n - 1] = nums2[n - 1]
+      n -= 1
+    end
+  end
+
+  while n.positive?
+    nums1[m + n - 1] = nums2[n - 1]
+    n -= 1
+  end
+
+  nums1
+end
